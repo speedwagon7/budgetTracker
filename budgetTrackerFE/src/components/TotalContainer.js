a2bfa7e8b92c67a9e5b3d8bd4e7ee21c270129ec
Thang Nguyen <thang.nguyen@fdmgroup.com>
2024-02-15 10:36:43 +0000

    fix merges due to relocation

---
+++ budgetTrackerFE/src/components/TotalContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TotalContainer = ({ content, className }) => {
+    return (
+        <div className={className}>{content}</div>
+    )
+}
+
+export default TotalContainer
