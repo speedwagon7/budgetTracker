4e5d30869fd59efa58722f01c320789ebd456577
Christian.Mutono <Christian.Mutono@fdmgroup.com>
2024-02-27 21:04:28 +0000

    page not found

--- budgetTrackerFE/src/Components/PageNotFound.jsx
+++ budgetTrackerFE/src/Components/PageNotFound.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import '../Styles/PageNotFound.css';
 
 const PageNotFound = () => {
   return (
-    <div>404PageNotFound</div>
+    <div className='page-not-found'>
+      <img src='https://miro.medium.com/v2/resize:fit:1358/0*QOZm9X5er1Y0r5-t' alt="404 - Page Not Found!"></img>
+    </div>
   )
 }
 
